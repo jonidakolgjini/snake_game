dcb007827133244f1b5e28d1bb746fbf30efd034
jonidakolgjini <88148962+jonidakolgjini@users.noreply.github.com>
2022-03-02 21:18:55 +0000

    update main.py
    update if statements for collisions with wall and tail

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("SNAKE")
-
 screen.tracer(0)
 
 snake = Snake()
@@ -26,25 +25,26 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
     # detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
-        scoreboard.increase_score()
         snake.extend()
+        scoreboard.increase_score()
 
-    # detect collision with wall
-    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-        scoreboard.reset()
-        snake.reset()
+    #Detect collision with wall.
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
-    # detect collision with tail
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            scoreboard.reset()
-            snake.reset()
+    #Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
